cd484e0c5f26ea847ce55cd44970e29e7c31482b
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-23 05:14:05 -0700

    multiplay 3 parmeters

---
+++ multiply.js
@@ -0,0 +1,7 @@
+// 1. write an arrow function that will take 3 parameters will multiply the parameters and will return the result
+const multiply =  (a, b, c) => {
+    return a * b * c;
+}
+const multi = multiply(2, 4, 6);
+console.log(multi);
+
